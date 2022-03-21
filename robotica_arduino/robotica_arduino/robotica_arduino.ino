9d3dafbb6f56cadf8eae7f36f66d34fb15b751b6
/ <447346@student.saxion.nl>
2022-03-21 23:23:28 +0100

    idk

--- robotica_arduino/robotica_arduino/robotica_arduino.ino
+++ robotica_arduino/robotica_arduino/robotica_arduino.ino
@@ -11,19 +11,21 @@
 		STATES
 -----------------------------*/
 
+#define ENABLE_SERIAL
+
 enum stepAndState : byte {
 	UNKOWN,
 	LEFT,
 	RIGHT,
-	END,
 	FORWARD,
 	TURNAROUND,
 	CHECKJUNCTION,
-	BACKWARD
+	BACKWARD,
+	FINISHED
 };
 
 /*----LETTERS FOR SGDP--- */
-byte digitData[14][7]{
+const byte digitData[14][7]{
 	{0,1,1,1,1,1,1},
 	{0,0,0,0,1,1,0},
 	{1,0,1,1,0,1,1},
@@ -52,11 +54,11 @@ byte digitData[14][7]{
 	INFRARED SENSOR
 -----------------------------*/
 
-const auto IR_OUTER_LEFT = A0;
-const auto IR_INNER_LEFT = A1;
-const auto IR_MIDDLE = A2;
-const auto IR_INNER_RIGHT = A3;
-const auto IR_OUTER_RIGHT = A4;
+constexpr auto IR_OUTER_LEFT = A0;
+constexpr auto IR_INNER_LEFT = A1;
+constexpr auto IR_MIDDLE = A2;
+constexpr auto IR_INNER_RIGHT = A3;
+constexpr auto IR_OUTER_RIGHT = A4;
 
 bool outerleft, innerleft, middle, innerright, outerright;
 
@@ -65,13 +67,13 @@ bool outerleft, innerleft, middle, innerright, outerright;
 *   MOTER LEFT
   _________________*/
   //PWM moter left E2
-const auto MOTER_LEFT_PWM = 6;
+constexpr auto MOTER_LEFT_PWM = 6;
 // moter direction (on forward) M2
-const auto MOTER_LEFT_DIRECTION = 7;
+constexpr auto MOTER_LEFT_DIRECTION = 7;
 // change direction 
 
 //SPEED
-int moter_left_speed;
+unsigned  moter_left_speed = 0;
 
 
 
@@ -79,18 +81,19 @@ int moter_left_speed;
 *   MOTER RIGHT
   -----------------------------*/
   // E1
-const auto MOTER_RIGHT_PWM = 5;
+constexpr auto MOTER_RIGHT_PWM = 5;
 //m1
-const auto MOTER_RIGHT_DIRECTION = 4;
+constexpr auto MOTER_RIGHT_DIRECTION = 4;;
 //speed
-int moter_right_speed;
+unsigned moter_right_speed = 0;
+bool goingForward = true;
 
 
 /*-----------------------------------
 *	ULTRA SOUND SENSOR
 	---------------------------------*/
-const int ECHO_PIN = 2;
-const int TRIG_PIN = 3;
+constexpr int ECHO_PIN = 2;
+constexpr int TRIG_PIN = 3;
 
 long duration; // variable for the duration of sound wave travel
 int distance; // variable for the distance measurement
@@ -103,11 +106,12 @@ int distance; // variable for the distance measurement
 ------------------------*/
 
 // number of encounters passed
-int encouter = 0;
+int encouter_count = 0;
 long lastEncounter;
 byte state;
 
-int juncPoss[3];
+/*set 0 to true for left 1 straight 2 forward if possible*/
+bool juncPoss[3] = {};
 
 
 
@@ -126,7 +130,12 @@ void setup() {
 	pinMode(IR_INNER_RIGHT, INPUT);
 	pinMode(IR_OUTER_RIGHT, INPUT);
 
+#ifdef ENABLE_SERIAL
 	Serial.begin(9600);
+#endif // ENABLE_SERIAL
+
+
+
 	lastEncounter = millis();
 	state = FORWARD;
 }
@@ -136,7 +145,7 @@ void loop() {
 
 	/*-------------UPDATE SENSORS------------*/
 		//updateUltraSoundDistance();
-
+	//---UPDATING THE SENSOR DELAYS FOR ~15 ms
 	digitalWrite(TRIG_PIN, LOW);
 	delayMicroseconds(2);
 	//
@@ -160,6 +169,10 @@ void loop() {
 	/*---------------
 		DO LOGICA
 		-------------*/
+
+	goingForward = true;
+
+
 	switch (state)
 	{
 	case FORWARD:
@@ -169,6 +182,19 @@ void loop() {
 			lastEncounter = millis();
 			state = CHECKJUNCTION;
 		}
+		else if (!(middle && innerleft && innerright)) {
+			lastEncounter = millis();
+			state = TURNAROUND;
+		}
+		else {
+			//Try to straighten;
+			if (!innerleft) {
+				moter_right_speed = 40;
+			}
+			else if (!innerleft) {
+				moter_right_speed = 40;
+			}
+		}
 		break;
 	case LEFT:
 		if (outerright || outerleft) {
@@ -177,7 +203,7 @@ void loop() {
 		}
 		else {
 			state = FORWARD;
-			encouter++;
+			encouter_count++;
 		}
 		break;
 	case RIGHT:
@@ -187,29 +213,66 @@ void loop() {
 		}
 		else {
 			state = FORWARD;
-			encouter++;
+			encouter_count++;
 		}
 		break;
 	case CHECKJUNCTION:
+		// first 100 mill check if we can go left and or right and keep driving forward
 		if (millis() - lastEncounter < 100) {
-
+			moter_left_speed = 60;
+			moter_right_speed = 60;
+			/*Check if we can go left and or right*/
+			juncPoss[0] = juncPoss[0] || !outerleft;
+			juncPoss[2] = juncPoss[2] || !outerright;
 		}
-
+		// after 100 millis check if we can go straight and go backwards
+		else {
+			juncPoss[1] = middle;
+			state = BACKWARD;
+		}
+		break;
+	case BACKWARD:
+		if (millis() - lastEncounter < 120) {
+			goingForward = false;
+			moter_left_speed = 60;
+			moter_right_speed = 60;
+		}
+		else {
+			if (juncPoss[0] && juncPoss[1] && juncPoss[2]) {
+				state = FINISHED;
+			}
+			if (juncPoss[0]) {
+				state = LEFT;
+			}
+			else if (juncPoss[1]) {
+				state = FORWARD;
+			}
+			else {
+				state = RIGHT;
+			}
+		}
+		break;
+	case FINISHED:
+		moter_left_speed = 0;
+		moter_right_speed = 0;
+		return;
+		//@TODO LED stuff
+		break;
+	case TURNAROUND:
+		moter_right_speed = 60;
+		moter_left_speed = 0;
+		if (millis() - lastEncounter > 100) {
+			if (middle) {
+				encouter_count++;
+				state = FORWARD;
+			}
+		}
 		break;
 	default:
 		break;
 	}
 
-
-
-
 	/*----------UPDATE MOTER----------*/
-
-
-
-
-
-
 
 	moter_left_speed = 60;
 	moter_right_speed = 60;
@@ -219,19 +282,24 @@ void loop() {
 		moter_left_speed = 0;
 		moter_right_speed = 0;
 	}
-	digitalWrite(MOTER_LEFT_DIRECTION, LOW);
-	digitalWrite(MOTER_RIGHT_DIRECTION, HIGH);
+	//set moter power;
+	digitalWrite(MOTER_LEFT_DIRECTION, !goingForward);
+	digitalWrite(MOTER_RIGHT_DIRECTION, goingForward);
 	analogWrite(MOTER_LEFT_PWM, moter_left_speed);   //PWM Speed Control
 	analogWrite(MOTER_RIGHT_PWM, moter_right_speed);   //PWM Speed Control
 
-	//Serial.print("Afstand: ");
-	//Serial.println(distance);
+#ifdef ENABLE_SERIAL
+	Serial.print("Afstand: ");
+	Serial.println(distance);
 
-	//for (int i = A0; i <= A4; i++) {
-	//	Serial.print(i);
-	//	Serial.print(": ");
-	//	Serial.println(digitalRead(i));
-	//}
+	for (int i = A0; i <= A4; i++) {
+		Serial.print(i);
+		Serial.print(": ");
+		Serial.println(digitalRead(i));
+	}
+#endif // ENABLE_SERIAL
+
+	
 
 
 }
